95a447e7f4db5ac10019ed94d3b62c9aaba10d57
balanand003 <balanandm01@gmail.com>
2017-06-28 14:38:30 +0530

    Update negative.c

--- negative.c
+++ negative.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+  clrscr();
+  printf("enter the num:");
+  scanf("%d",&n);
+  if(n>0)
+     {
+    printf("the number is positive %d",n);
+  }
+  else if(n==0)
+  {
+    printf("the number is zero %d",n);
+  }
+  else
+  {
+    printf("the number is negative %d",n);
+  }
+  getch();
+}
