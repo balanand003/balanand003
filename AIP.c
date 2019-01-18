5f1057d9f4988862d9e884a617108637dfa1963b
balanand003 <balanandm01@gmail.com>
2019-01-18 11:31:23 +0530

    Create AIP.c

---
+++ AIP.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int  i,num,diff,firstterm,sum=0;
+printf("enter the numberofseries,firstterm,difference");
+scanf("%d\t%d\t%d",&number,&firstterm,&difference);
+for(i=0;i<number;i++)
+{
+sum=sum+num;
+firstterm=+diff;
+}
+printf("\n%d",sum);
+getch();
+}
