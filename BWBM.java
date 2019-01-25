a5f8603fb814bc536cc18a9dcddfefb662b9d9bc
balanand003 <balanandm01@gmail.com>
2019-01-25 12:09:50 +0530

    Create BWBM.java

---
+++ BWBM.java
@@ -0,0 +1,12 @@
+package str;
+import java.util.Scanner;
+public class Counter {
+public static void main(String[] args)
+{
+int n;
+System.out.println("Enter the number");
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+System.out.println(n);
+}
+}
