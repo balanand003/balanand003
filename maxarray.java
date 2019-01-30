bd27c57e3381ddf590b3e64a9a68dba2f6544bd0
balanand003 <balanandm01@gmail.com>
2019-01-30 14:29:39 +0530

    Create maxarray.java

---
+++ maxarray.java
@@ -0,0 +1,25 @@
+class FindMaximum  
+{ 
+int maxIndexDiff(int arr[], int n)  
+{ 
+int maxDiff = -1; 
+int i, j; 
+for (i = 0; i < n; ++i)  
+{ 
+for (j = n - 1; j > i; --j)  
+{ 
+if (arr[j] > arr[i] && maxDiff < (j - i)) 
+maxDiff = j - i; 
+} 
+} 
+return maxDiff; 
+} 
+public static void main(String[] args)  
+{ 
+FindMaximum max = new FindMaximum(); 
+int arr[] = {9, 2, 3, 4, 5, 6, 7, 8, 18, 0}; 
+int n = arr.length; 
+int maxDiff = max.maxIndexDiff(arr, n); 
+System.out.println(maxDiff); 
+} 
+} 
