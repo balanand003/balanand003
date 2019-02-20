5956710f0bf03c194d9f7ccb2ff1fd29e2671115
balanand003 <balanandm01@gmail.com>
2019-02-20 14:10:45 +0530

    Create repeat1.py

---
+++ repeat1.py
@@ -0,0 +1,11 @@
+def Repeat(x):
+    size=len(x)
+    repeated=[]
+    for i in range(size):
+         k=i+1
+         for j in range(k,size):
+              if x[i]==x[j] and x[i] not in repeated:
+                    repeated.append(x[i])
+         return repeated
+     list1=[1,2,3,2,1]
+     print(Repeat(list1))
