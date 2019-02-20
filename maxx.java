80d5a14582e38190163bfc5882f4e138087b24f3
balanand003 <balanandm01@gmail.com>
2019-02-20 14:50:59 +0530

    Create maxx.java

---
+++ maxx.java
@@ -0,0 +1,11 @@
+import java.util.Arrays; 
+public class big 
+{ 
+public static void main(String[] args) 
+{ 
+int[] arr = {13, 45, 101, 102}; 
+Arrays.sort(arr); 
+System.out.printf("Modified arr[] : %s", 
+Arrays.toString(arr)); 
+} 
+} 
