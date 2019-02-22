d73f9e81572f5336caef02758cb97c384a0eac0a
balanand003 <balanandm01@gmail.com>
2019-02-22 10:39:22 +0530

    Create child.py

---
+++ child.py
@@ -0,0 +1,19 @@
+def max(1):
+            m=-1
+            for i in 1:
+                        if m<i:
+                                m=i
+            print(m)
+            return m
+def main():
+            n=int(input())
+            1=[]
+            f=0
+            for i in range(n):
+                              1.append(int(input()))
+            m=max)1)
+            print(m+n)
+main()            
+            
+            
+        
