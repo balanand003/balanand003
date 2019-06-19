efc21384ed9073c9d75779f6d8146ee9aa62c6ee
balanand003 <balanandm01@gmail.com>
2019-06-19 14:17:16 +0530

    Create maximum123.py

---
+++ maximum123.py
@@ -0,0 +1,17 @@
+A=int(input())
+B=list(map(int,input().split()))
+C=[]
+D[]
+for i in range(len(B)):
+    if(i%2==0):
+        C.append(B[i])
+    else:
+        D.append(B[i])
+for j in C:
+    d=sum(C)
+for k in D:
+    f=sum(D)
+if(d>f):
+    print(d)
+else:
+    print(f)
