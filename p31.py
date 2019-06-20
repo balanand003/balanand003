c17e2f96b3e2bbd0fd78173fa5b512d731480415
balanand003 <balanandm01@gmail.com>
2019-06-20 16:12:13 +0530

    Update and rename p21.py to p31.py

--- p31.py
+++ p31.py
@@ -4,15 +4,15 @@ for i in range(0,min(A,B)):
     d=list(map(int,input().split()))
    C.append(d)
 for i in range(0,min(A,B)):
-    r=[]
+    x=[]
     for j in range(0,len(C[i])):
         r.append(C[i][j])
     C[i]=(sorted(r))
 for i in range(0,max(A,B)):
-    r=[]
+    x=[]
     for j in range(0,min(A,B)):
-        r.append(C[j][i])
-    r=sorted(r)
-    for l in range(min(A,B)): C[l][i]=r[l]
+        x.append(C[j][i])
+    x=sorted(x)
+    for l in range(min(A,B)): C[l][i]=x[l]
 for i in range(0,min(A,B)):
     print(*C[i])
