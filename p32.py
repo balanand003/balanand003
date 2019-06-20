7b840aa498e62202cb31700e73b73be758c56149
balanand003 <balanandm01@gmail.com>
2019-06-20 16:18:14 +0530

    Update p32.py

--- p32.py
+++ p32.py
@@ -1,12 +1,6 @@
-A=input()
-B=len(A)
-C=[]
-r=""
-for i in A:
-    C.append(i)
-for i in range(B):
-    r=r+l.pop()
-if r==A:
-    print('YES')
+A=raw_input()
+rev=A[::-1]
+if(rev==A):
+    print("YES")
 else:
-    print('NO')
+    print("NO")
