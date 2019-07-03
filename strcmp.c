d1f4934b67071cee9fdac5f2befe1e368fc16df6
balanand003 <balanandm01@gmail.com>
2019-07-03 16:21:02 +0530

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s1[10],s2[10],j;
+clrscr();
+printf("enter the string:");
+scanf("%s",s1);
+for(j=0;s[j]!='\0';++j)
+{
+s2[j]=s1[j]
+}
+s2[j]='\0';
+printf("string is:%s",s2);
+getch();
+}
