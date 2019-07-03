b6c9c7de982ea706ccf8e53f6f9f676c20dd75bf
balanand003 <balanandm01@gmail.com>
2019-07-03 18:19:18 +0530

    Create dhoni.py

---
+++ dhoni.py
@@ -0,0 +1,13 @@
+A=input()
+B="dhoni"
+C=0
+if(len(A)==5):
+    for i in range(0,len(B)):
+        if(A[i] in B):
+            C=C+1
+    if(C==5):
+        print("no")
+    else:
+        print("no")
+else:
+    print("no")
