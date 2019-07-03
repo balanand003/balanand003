e62a322cff55f7c9d60aa4d141dcd73e588a60f8
balanand003 <balanandm01@gmail.com>
2019-07-03 16:23:30 +0530

    Update strcpy.c

--- strcpy.c
+++ strcpy.c
@@ -4,13 +4,13 @@ void main()
 {
 char s1[10],s2[10],j;
 clrscr();
-printf("enter the string:");
+printf("\nenter the string:");
 scanf("%s",s1);
 for(j=0;s[j]!='\0';++j)
 {
 s2[j]=s1[j]
 }
 s2[j]='\0';
-printf("string is:%s",s2);
+printf("\nstring is:%s",s2);
 getch();
 }
