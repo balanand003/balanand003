d4234ca0b95c3d2f72770b8a9e682d70d922fa3f
balanand003 <balanandm01@gmail.com>
2019-07-03 16:45:43 +0530

    Update range1.py

--- range1.py
+++ range1.py
@@ -1,7 +1,7 @@
 def r1(n):
     if n in range(1,10):
-        print("%s is in the range"%str(n))
+        print("yes"%str(n))
     else:
-        print("outside the region. ")
+        print("no ")
 r1(3)
         
