aaa649edfc6244bfa6b0e7f7738545228006779b
balanand003 <balanandm01@gmail.com>
2019-07-03 18:26:06 +0530

    Create line.py

---
+++ line.py
@@ -0,0 +1,7 @@
+A,B=list(map(int,input().split()))
+C,D=list(map(int,input().split()))
+E,F=list(map(int,input().split()))
+if (A==C==E or B==D==F or(A==B and C==d and E==F)):
+    print ("yes") 
+else: 
+    print ("no")
