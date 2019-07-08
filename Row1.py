8a7959eefcec3d7da6388c5e6ad138d003194e69
balanand003 <balanandm01@gmail.com>
2019-07-08 18:04:18 +0530

    Update and rename Row.py to Row1.py

--- Row1.py
+++ Row1.py
@@ -1,28 +1,28 @@
 A,B=input().split()
 A=int(A)
 B=int(B)
-s=''
-u=2
+p=''
+q=2
 if(A+B<=3):
     for i in range(0,A+B):
         if(i%2!=0):
-            s=s+'0'
+            p=p+'0'
         else:
-            s=s+'1'
+            p=p+'1'
 else:    
     for i in range(0,A+B):
-        if(i==u):
-            s=s+'0'
-            if(u==B):
-                u=u+2
+        if(i==q):
+            p=p+'0'
+            if(q==B):
+                q=q+2
             else:
-                u=u+3
+                q=q+3
         else:
-            s=s+'1'
-x=len(s)-1
-if(int(s[x])==0):
+            p=p+'1'
+x=len(p)-1
+if(int(p[x])==0):
     print('-1')
 elif A==1 and B==2: 
      print("011")
 else:
-    print(s)
+    print(p)
