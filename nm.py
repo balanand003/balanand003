724d9029f34f9fb56dd91b8eaab1ec3ac4fcf981
balanand003 <balanandm01@gmail.com>
2019-07-22 15:55:22 +0530

    Update nm.py

--- nm.py
+++ nm.py
@@ -5,7 +5,7 @@ for i in range(A1,B1):
        if(V==1):
             continue
        for i in range(2,V):
-               if(r%i==0):
+               if(V%i==0):
                    flag=False
                    break
        else:
