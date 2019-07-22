e9d9e22dae87d3a848ffbd0647218227edd1f043
balanand003 <balanandm01@gmail.com>
2019-07-22 15:30:49 +0530

    Rename matrix.py to matrix123.py

---
+++ matrix123.py
@@ -0,0 +1,21 @@
+A1=int(input())
+B1=[]
+C1=0
+for i in range(A1):
+  B1.append(list(map(int,input().split())))
+res=[]
+for i in range(n+1):
+  if i==0:
+    res.append(list("0"*(A1+2)))
+  else:
+    s="0"
+    for j in range(A1):
+      s=s+str(B1[i-1][j])
+    res.append(list(s+"0"))
+res.append(list("0"*(A1+2)))
+for i in range(len(res)):
+  for j in range(len(res)):
+    if res[i][j]=="1" and res[i-1][j]==res[i+1][j]==res[i][j-1]==res[i][j+1]==res[i+1][j+1]==res[i+1][j-1]==res[i-1][j+1]==res[i-1][j-1]=="0":
+
+      C1=C1+1
+print(C1)
