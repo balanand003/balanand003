701b5bf42bef9e06314ebe5709a27840c5ced34f
balanand003 <balanandm01@gmail.com>
2019-07-22 15:51:14 +0530

    Create nm.py

---
+++ nm.py
@@ -0,0 +1,13 @@
+A1,B1=list(map(int,input().split(" ")))
+C1=0
+for i in range(A1,B1):
+       V=(bin(i)[2:]).count("1")
+       if(V==1):
+            continue
+       for i in range(2,V):
+               if(r%i==0):
+                   flag=False
+                   break
+       else:
+          C1=C1+1
+print(C1)
